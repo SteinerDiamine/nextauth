45c9538cc1949dceaf8b191a9d15ee17002375ff
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-10-06 18:42:34 +0530

    commit login

--- components/auth/lofin-form.tsx
+++ components/auth/lofin-form.tsx
@@ -8,6 +8,9 @@ import { LoginSchema } from "@/schemas";
 import {
   Form , FormControl, FormField, FormItem,FormLabel,FormMessage
 } from '@/components/ui/form'
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
 
 
 
@@ -20,6 +23,11 @@ export const  LoginForm = () => {
       password:'',
     }
   })
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+      console.log(values);
+      
+  }
   return(
     <CardWrapper 
        headerLabel="Welcome back"
@@ -27,7 +35,63 @@ export const  LoginForm = () => {
       backButtonLabel= "Don't have an account" 
       showSocial
       >
-      Login form
+      <Form {...form}>
+          <form 
+           onSubmit={form.handleSubmit(onSubmit)}
+           className="space-y-6"> 
+           <div className="space-y-4">
+            <FormField 
+              control={form.control}
+              name='email'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Email
+                  </FormLabel>
+
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="diamine123@gmail.com"
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage/>
+                </FormItem>
+              )} 
+              />
+
+
+          <FormField 
+              control={form.control}
+              name='password'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Password
+                  </FormLabel>
+
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Enter your password"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage/>
+                </FormItem>
+              )} 
+              />
+
+
+           </div>
+           <Button type="submit"
+           className="w-full bg-gray-900">
+            Login
+           </Button>
+
+          </form>
+      </Form>
     </CardWrapper>
   )
 }
